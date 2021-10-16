1ae09f99634559c2cb8e817e85918c0829d68770
Jesse <jlutannn@gmail.com>
2021-10-16 00:48:14 -0700

    type fixes, added new speaker, updates to schedule

--- src/components/about-us.jsx
+++ src/components/about-us.jsx
@@ -1,8 +1,9 @@
 import React from "react"
+import { graphql, StaticQuery } from "gatsby"
+
+import AboutData from "../data/homeData.json"
 import aboutUsStyles from "../styles/about-us.module.css"
 import Image from "../components/image"
-
-import { graphql, StaticQuery } from "gatsby"
 
 /**
  * Contains floating bubbles with short blobs of info about the club
@@ -10,7 +11,7 @@ import { graphql, StaticQuery } from "gatsby"
  *
  * Receives no props
  */
-export default class AboutUs extends React.Component {
+class AboutUs extends React.Component {
   constructor(props) {
     super()
     this.state = { width: 0, height: 0 }
@@ -32,74 +33,78 @@ export default class AboutUs extends React.Component {
 
   render() {
     return (
-      <StaticQuery
-        query={graphql`
-          query AboutUsSectionData {
-            allDataJson {
-              nodes {
-                about_us_main
-                about_us_off_1
-                about_us_off_2
-                about_us_off_3
-                about_us_off_4
-                about_us_off_5
-                about_us_off_6
-              }
-            }
-          }
-        `}
-        render={data => (
-          <div className={aboutUsStyles.infoRow}>
-            <div>
-              <div
-                className={`${aboutUsStyles.secondaryInfo} ${aboutUsStyles.topLeft}`}
-              >
-                {data.allDataJson.nodes[1].about_us_off_1}
-              </div>
-              <div
-                className={`${aboutUsStyles.secondaryInfo} ${aboutUsStyles.midLeft}`}
-              >
-                {data.allDataJson.nodes[1].about_us_off_2}
-              </div>
-              <div
-                className={`${aboutUsStyles.secondaryInfo} ${aboutUsStyles.bottomLeft}`}
-              >
-                {data.allDataJson.nodes[1].about_us_off_3}
-              </div>
+      // <StaticQuery
+      //   query={graphql`
+      //     query AboutUsSectionData {
+      //       allDataJson {
+      //         nodes {
+      //           about_us_main
+      //           about_us_off_1
+      //           about_us_off_2
+      //           about_us_off_3
+      //           about_us_off_4
+      //           about_us_off_5
+      //           about_us_off_6
+      //         }
+      //       }
+      //     }
+      //   `}
+      //   render={data => (
+      //   )}
+      // />
+      <React.Fragment>
+        <div className={aboutUsStyles.infoRow}>
+          <div>
+            <div
+              className={`${aboutUsStyles.secondaryInfo} ${aboutUsStyles.topLeft}`}
+            >
+              {AboutData["about-us-off-1"]}
             </div>
             <div
-              style={{
-                width: (this.state.width * 0.7).toString() + "px",
-                height: (this.state.width * 0.7).toString() + "px",
-                minHeight: "335px",
-                minWidth: "335px",
-                maxWidth: "450px",
-                maxHeight: "450px",
-              }}
-              className={aboutUsStyles.mainInfo}
+              className={`${aboutUsStyles.secondaryInfo} ${aboutUsStyles.midLeft}`}
             >
-              {data.allDataJson.nodes[1].about_us_main}
+              {AboutData["about-us-off-2"]}
             </div>
-            <div>
-              <div
-                className={`${aboutUsStyles.secondaryInfo} ${aboutUsStyles.topRight}`}
-              >
-                {data.allDataJson.nodes[1].about_us_off_4}
-              </div>
-              <div
-                className={`${aboutUsStyles.secondaryInfo} ${aboutUsStyles.midRight}`}
-              >
-                {data.allDataJson.nodes[1].about_us_off_5}
-              </div>
-              <div
-                className={`${aboutUsStyles.secondaryInfo} ${aboutUsStyles.bottomRight}`}
-              >
-                {data.allDataJson.nodes[1].about_us_off_6}
-              </div>
+            <div
+              className={`${aboutUsStyles.secondaryInfo} ${aboutUsStyles.bottomLeft}`}
+            >
+              {AboutData["about-us-off-3"]}
             </div>
           </div>
-        )}
-      />
+          <div
+            style={{
+              width: (this.state.width * 0.7).toString() + "px",
+              height: (this.state.width * 0.7).toString() + "px",
+              minHeight: "335px",
+              minWidth: "335px",
+              maxWidth: "450px",
+              maxHeight: "450px",
+            }}
+            className={aboutUsStyles.mainInfo}
+          >
+            {AboutData["about-us-main"]}
+          </div>
+          <div>
+            <div
+              className={`${aboutUsStyles.secondaryInfo} ${aboutUsStyles.topRight}`}
+            >
+              {AboutData["about-us-off-4"]}
+            </div>
+            <div
+              className={`${aboutUsStyles.secondaryInfo} ${aboutUsStyles.midRight}`}
+            >
+              {AboutData["about-us-off-5"]}
+            </div>
+            <div
+              className={`${aboutUsStyles.secondaryInfo} ${aboutUsStyles.bottomRight}`}
+            >
+              {AboutData["about-us-off-6"]}
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
     )
   }
 }
+
+export default AboutUs
